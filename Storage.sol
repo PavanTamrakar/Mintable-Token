218fe226ca9ea1f82b5b573fe55c197054405aea
Pavan Tamrakar <72463719+PavanTamrakar@users.noreply.github.com>
2022-05-29 13:42:05 +0530

    Create Storage.sol

---
+++ Storage.sol
@@ -0,0 +1,14 @@
+pragma solidity >= 0.4.0 < 0.8.0;
+
+contract Storage{
+
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
